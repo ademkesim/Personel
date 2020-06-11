af5f56fc3e2275a7daa0299504c3f18b07ce0e9f
Adem Kesim <adem.kesim@poldy.com.tr>
2020-06-11 11:37:34 +0300

    permanager eklendi

---
+++ Business/Concrete/PerManager.cs
@@ -0,0 +1,44 @@
+﻿using Business.Abstract;
+using Business.Constans;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    class PerManager : IPerService
+    {
+        private IPerDal _perDal;
+
+        public PerManager(IPerDal perDal)
+        {
+            _perDal = perDal;
+        }
+
+        public IResult Add(Per per)
+        {
+            _perDal.Add(per);
+            return new SuccessResult(Messages.PersonAdded);
+        }
+
+        public IResult Update(Per per)
+        {
+            _perDal.Update(per);
+            return new SuccessResult(Messages.PersonUpdated);
+        }
+        public IResult Delete(Per per)
+        {
+            _perDal.Delete(per);
+            return new SuccessResult(Messages.PersonDeleted);
+        }
+
+        public IDataResult<List<Per>> GetList()
+        {
+            return new SuccessDataResult<List<Per>>(_perDal.GetList());
+        }
+
+    }
+}
---
+++ Per01API/Controllers/AuthController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Entities.Concreate;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PoldyCvAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+
+        [HttpPost("login")]
+        public IActionResult Login(UserForLoginDto userForLoginDto)
+        {
+            var userToLogin = _authService.Login(userForLoginDto);
+            if (!userToLogin.Success)
+            {
+                return BadRequest(userToLogin.Message);
+            }
+
+            var result = _authService.CreateAccessToken(userToLogin.Data);
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register(UserForRegisterDto userForRegisterDto)
+        {
+            var userExsits = _authService.UserExists(userForRegisterDto.Mail);
+            if (!userExsits.Success)
+            {
+                return BadRequest(userExsits.Message);
+            }
+
+            var registerResult = _authService.Register(userForRegisterDto, userForRegisterDto.Password);
+            var result = _authService.CreateAccessToken(registerResult.Data);
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+
+            return BadRequest(result.Message);
+
+        }
+    }
+}
