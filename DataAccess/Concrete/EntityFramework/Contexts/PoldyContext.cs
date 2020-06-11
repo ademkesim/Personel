4148d2e99dc2f2434a250a612e734de205d5c0a1
Adem Kesim <adem.kesim@poldy.com.tr>
2020-06-11 09:52:16 +0300

    Dataaccess katmanı kodlandı

--- DataAccess/Concrete/EntityFramework/Contexts/PoldyContext.cs
+++ DataAccess/Concrete/EntityFramework/Contexts/PoldyContext.cs
@@ -6,5 +6,10 @@ namespace DataAccess.Concrete.EntityFramework.Contexts
 {
     public class PoldyContext:DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-M6AH6CF;initial catalog=poldy;integrated security=true");
+        }
+        public DbSet<Per> Pers { get; set; }
     }
 }
---
+++ DataAccess/Concrete/EntityFramework/EfPerDal.cs
@@ -0,0 +1,12 @@
+﻿using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfPerDal:EfEntityRepositoryBase<Per, PoldyContext>, IPerDal
+    {
+    }
+}
