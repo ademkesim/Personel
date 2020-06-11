cb9a82f1cdde092813fff4c502dd2f91373d2433
Adem Kesim <adem.kesim@poldy.com.tr>
2020-06-11 10:25:40 +0300

    Core Katmanı tamamlandı

---
+++ Core/Security/Jwt/AccessToken.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Entities.Concrete;
+
+namespace Core.Security.Jwt
+{
+    public class AccessToken
+    {
+        public string Token { get; set; }
+        public DateTime Expiration { get; set; }
+        public Person Person { get; set; }
+    }
+}
