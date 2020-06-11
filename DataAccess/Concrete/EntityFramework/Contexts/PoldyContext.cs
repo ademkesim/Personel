6115005c11fdc73ff3989c71370eec23e8988528
Adem Kesim <adem.kesim@poldy.com.tr>
2020-06-11 09:39:09 +0300

    Katmanlar oluşturuldu

---
+++ DataAccess/Concrete/EntityFramework/Contexts/PoldyContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+namespace DataAccess.Concrete.EntityFramework.Contexts
+{
+    public class PoldyContext:DbContext
+    {
+    }
+}
